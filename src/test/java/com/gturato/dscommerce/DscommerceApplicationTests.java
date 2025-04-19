5937e6649377e7265a1ac2e6bcdde6f97e3873fe
Gabriel Turato <gabrielturato@outlook.com>
2025-04-19 13:48:23 -0300

    First Commit

---
+++ src/test/java/com/gturato/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gturato.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
