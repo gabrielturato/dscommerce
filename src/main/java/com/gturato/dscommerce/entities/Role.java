f7919dd6e8f93422034602ab77f4db47ab212bf4
Gabriel Turato <gabrielturato@outlook.com>
2025-04-19 14:06:54 -0300

    Entities

---
+++ src/main/java/com/gturato/dscommerce/entities/Role.java
@@ -0,0 +1,39 @@
+package com.gturato.dscommerce.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return "";
+    }
+}
