708f8a3159ebd171592179780b3d269910be5eb8
Gabriel Turato <gabrielturato@outlook.com>
2025-04-21 19:52:44 -0300

    Access control for endpoints and users/me endpoint

--- src/main/java/com/gturato/dscommerce/entities/User.java
+++ src/main/java/com/gturato/dscommerce/entities/User.java
@@ -99,7 +99,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles;
     }
 
     public String getPassword() {
@@ -113,22 +113,22 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 
 
