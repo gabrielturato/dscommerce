8a53ce0247c735a624b9e2a3f5e668b6a6712226
Gabriel Turato <gabrielturato@outlook.com>
2025-05-19 22:56:30 -0300

    Setting acknowledge() for exceptions

--- src/main/java/com/gturato/dscommerce/services/PaymentService.java
+++ src/main/java/com/gturato/dscommerce/services/PaymentService.java
@@ -6,6 +6,7 @@ import com.gturato.dscommerce.entities.OrderStatus;
 import com.gturato.dscommerce.entities.Payment;
 import com.gturato.dscommerce.repositories.OrderRepository;
 import com.gturato.dscommerce.repositories.PaymentRepository;
+import com.gturato.dscommerce.services.exceptions.AlreadyHasPaymentException;
 import com.gturato.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,11 +27,12 @@ public class PaymentService {
     public void insertPayment(PaymentListenerDTO dto){
         Order order = orderRepository.findById(dto.getOrderId()).orElseThrow(() -> {
                     LOG.error("Doesn't exist a order for this payment");
-                    return new ResourceNotFoundException("Recurso nao encontrado");
+                    return new ResourceNotFoundException("Resource not found");
                 });
 
         if(order.getPayment() != null){
             LOG.error("Order already has a payment, paymentID {}",order.getPayment().getId());
+            throw new AlreadyHasPaymentException("Order has a payment already.");
         }
 
         order.setStatus(OrderStatus.PAID);
